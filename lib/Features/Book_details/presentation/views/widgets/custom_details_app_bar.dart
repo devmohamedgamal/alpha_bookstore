b65a9101de19fcabc83fabdf3883f71b79db5b68
devmohamedgamal <mpedo2010@gmail.com>
2023-10-14 08:14:17 +0300

    finsh favorite icon in details view

--- lib/Features/Book_details/presentation/views/widgets/custom_details_app_bar.dart
+++ lib/Features/Book_details/presentation/views/widgets/custom_details_app_bar.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class CustomDetailsAppBar extends StatelessWidget {
+class CustomDetailsAppBar extends StatefulWidget {
   const CustomDetailsAppBar({super.key});
 
+  @override
+  State<CustomDetailsAppBar> createState() => _CustomDetailsAppBarState();
+}
+
+class _CustomDetailsAppBarState extends State<CustomDetailsAppBar> {
+  int selctedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -18,11 +24,22 @@ class CustomDetailsAppBar extends StatelessWidget {
             )),
         const Spacer(),
         IconButton(
-            onPressed: () {},
-            icon: const Icon(
+            onPressed: () {
+              if (selctedIndex == 0) {
+                setState(() {
+                  selctedIndex = 1;
+                });
+              } else {
+                setState(() {
+                  selctedIndex = 0;
+                });
+              }
+            },
+            icon: Icon(
               Icons.favorite,
-              size: 30,
-              color: Colors.red,
+              size: 40,
+              color:
+                  selctedIndex == 0 ? Colors.grey.withOpacity(0.4) : Colors.red,
             )),
       ],
     );
